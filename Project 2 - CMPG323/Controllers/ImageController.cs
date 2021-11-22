f9dc6b62ed0f9459d81de1d254326e3d742f510a
HlulaniR <71436045+HlulaniR@users.noreply.github.com>
2021-11-22 21:45:20 +0200

    commit

---
+++ Project 2 - CMPG323/Controllers/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project_2___CMPG323.Models;
+
+namespace Project_2___CMPG323.Controllers
+{
+    public class ImageController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Image image)
+        {
+            return View();
+        }
+    }
+}
